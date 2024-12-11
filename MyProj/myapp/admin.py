7641acc9906924e7b0b346f418f9245b0d52354d
Andrey <Bazulin.a.i@gmail.com>
2024-12-11 14:36:41 +0300

    f

---
+++ MyProj/myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(News)
+admin.site.register(Buyer)
+admin.site.register(Game)
+admin.site.register(TestTable)
